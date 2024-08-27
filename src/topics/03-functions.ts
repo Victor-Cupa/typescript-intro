6d0bd24d9777ca6eb9302e7483639e01f87da69f
VictorCupa <victorcadena60780@gmail.com>
2024-08-26 21:03:02 -0500

    :pencil2: TypeScript introducction :pencil2:

--- src/topics/03-functions.ts
+++ src/topics/03-functions.ts
@@ -1,4 +1,27 @@
 
+function addNumbers (a:number, b: number){
+    return a + b;
+}
+
+const result:number = addNumbers(1,2);
+console.log({result});
 
 
+// arrow functions o funcion flecha
+
+const addNumbersArrow = (a:number, b:number):string => {
+    return `${a + b}`;
+};
+
+const result2 = addNumbersArrow(4,4);
+console.log({result2});
+
+
+// multiply functions
+
+function multiply (firstnumber:number, secondnumber?:number, base:number = 6){
+    return firstnumber * base;
+}
+const multiplyResult = multiply(4);
+console.log({multiplyResult});
 export {}
